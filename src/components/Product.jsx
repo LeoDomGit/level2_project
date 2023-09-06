d82e5f9c07beea9f4ecb09b8facc92c0e7727b3d
LeoDomGit <leodomsolar@gmail.com>
2023-09-06 21:17:37 +0700

    C

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,18 +3,27 @@ import React from 'react'
 function Product(props) {
   return (
     <div>
-        <div className="row" >
-        <div  className="card" style={{"width": "97%","background":"white","fontFamily":"Times New Roman"}}>
-            <img src="https://shopdunk.com/images/thumbs/0009181_iphone-14-128gb_240.png" className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">{props.name}</h5>
-                <p className="card-text">
-                {props.price}
-                </p>
-                <a href="#" className="btn btn-primary">Xem thêm</a>
+      <div className="row" >
+        <div className="card" style={{ "width": "97%", "background": "white", "fontFamily": "Times New Roman", "boxShadow": "2px 2px 2px 2px grey" }}>
+          <img src="https://shopdunk.com/images/thumbs/0009181_iphone-14-128gb_240.png" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title text-center">{props.name}</h5>
+            <p className="card-text text-center">
+              {props.price}
+            </p>
+            <div className="row">
+              <div className="col-md">
+                <a href="#" className="btn btn-primary w-100">Xem thêm</a>
+              </div>
+              <div className="col-md">
+                <a href="#" className="btn btn-primary w-100">Xem thêm</a>
+
+              </div>
             </div>
-            </div>
+
+          </div>
         </div>
+      </div>
     </div>
   )
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,31 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from "../components/Navbar";
 import Product from '../components/Product';
+import "../css/products.css";
 function Products() {
-  // const [cates,setCate]= useState([]);
-  // const [brands,setBrand]= useState([]);
+  const [products,setProducts]= useState([]);
 
-  // const getValue = async ()=>{
-  //   const result = await fetch('https://students.trungthanhweb.com/api/home?apitoken='+localStorage.getItem('token'))
-  //   .then((res)=>res.json());
-  //   if(result.categrories){
-  //   setCate(result.categrories);
-  //   }
-  //   if(result.brands){
-  //     setBrand(result.brands);
-  //   }
+  const getValue = async ()=>{
+    const result = await fetch('https://students.trungthanhweb.com/api/home?apitoken='+localStorage.getItem('token'))
+    .then((res)=>res.json());
+    if(result.products.length>0){
+      console.log(result.products);
+    }
 
-  // }
-  // useEffect(()=>{
-  //   if(localStorage.getItem('token')&& localStorage.getItem("token")!=null){
-  //     getValue();
-  //   }
-  // },[])
+  }
+  useEffect(()=>{
+    if(localStorage.getItem('token')&& localStorage.getItem("token")!=null){
+      getValue();
+    }
+  },[])
 
   return (
     <>
     <Navbar/>
-    <div className='container'>
+    <div className='containerProduct'>
 
       <div className="row mt-4">
           <div className="col-md-3">
