be1139bfad63efb6edfa69407793d9fe23c23b9e
LeoDomGit <leodomsolar@gmail.com>
2023-09-07 08:01:06 +0700

    C

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,19 +4,19 @@ function Product(props) {
   return (
     <div>
       <div className="row" >
-        <div className="card" style={{ "width": "97%", "background": "white", "fontFamily": "Times New Roman", "boxShadow": "2px 2px 2px 2px grey" }}>
-          <img src="https://shopdunk.com/images/thumbs/0009181_iphone-14-128gb_240.png" className="card-img-top" alt="..." />
+        <div className="card" style={{ "width": "97%", "background": "white", "fontFamily": "Times New Roman", "boxShadow": "1px 1px 1px 2px grey" }}>
+          <img src={props.image} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title text-center">{props.name}</h5>
             <p className="card-text text-center">
-              {props.price}
+              {Intl.NumberFormat('en-US').format(props.price)}
             </p>
             <div className="row">
               <div className="col-md">
                 <a href="#" className="btn btn-primary w-100">Xem thêm</a>
               </div>
               <div className="col-md">
-                <a href="#" className="btn btn-primary w-100">Xem thêm</a>
+                <a href="#" className="btn btn-warning w-100">Thêm </a>
 
               </div>
             </div>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,39 +3,50 @@ import Navbar from "../components/Navbar";
 import Product from '../components/Product';
 import "../css/products.css";
 function Products() {
-  const [products,setProducts]= useState([]);
+  const [products, setProducts] = useState([]);
+  const [nextLink, setnextLink] = useState('https://students.trungthanhweb.com/api/home');
+  const image = `https://students.trungthanhweb.com/images/`;
 
-  const getValue = async ()=>{
-    const result = await fetch('https://students.trungthanhweb.com/api/home?apitoken='+localStorage.getItem('token'))
-    .then((res)=>res.json());
-    if(result.products.length>0){
-      console.log(result.products);
-    }
+  const getValue = () => {
+     fetch(nextLink + '?apitoken=' + localStorage.getItem('token'))
+      .then((res) => res.json()).then((result)=>{
+        if (result.products.data.length > 0) {
+          setProducts(result.products.data);
+          if (result.products.next_page_url == null) {
+            setnextLink(result.products.first_page_url);
+          } else {
+            setnextLink(result.products.next_page_url);
+          }
+        }
+      })
+    
 
   }
-  useEffect(()=>{
-    if(localStorage.getItem('token')&& localStorage.getItem("token")!=null){
-      getValue();
-    }
-  },[])
+  const loadMore = ()=>{
+    getValue();
+  }
+  useEffect(() => {
+    getValue();
+  }, [])
 
   return (
     <>
-    <Navbar/>
-    <div className='containerProduct'>
+      <Navbar />
+      <div className='containerProduct'>
+        {products && products.length > 0 &&
+          <div className="row mt-4">
+            {products.map((item) =>
+              <div className="col-md-3">
+                <Product image={image + item.images} name={item.name} price="18000000" />
+              </div>
+            )}
 
-      <div className="row mt-4">
-          <div className="col-md-3">
-          <Product name="IPhone X" price="18000000"/>
           </div>
-          <div className="col-md-3">
-          <Product name="IPhone 11" price="19000000"/>
-          </div>
-          <div className="col-md-3">
-          <Product name="IPhone 12" price="20000000"/>
-          </div>
-          <div className="col-md-3">
-          <Product name="IPhone XS" price="14000000"/>
+
+        }
+        <div className="row mt-3 text-center">
+          <div className="col-md">
+            <button class="btn btn-primary" onClick={loadMore}>Xem thêm</button>
           </div>
         </div>
       </div>
