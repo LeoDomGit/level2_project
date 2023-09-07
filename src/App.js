9c3e19b0d0b7a21c6c069fb6badaef452b7786fa
LeoDomGit <leodomsolar@gmail.com>
2023-09-07 09:23:19 +0700

    C

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Todo from "./pages/Todo";
 import Products from "./pages/Products";
 import Login from "./pages/Login";
 import Todo1 from "./pages/Todo1";
+import Page404 from "./pages/Page404";
+
 function App() {
 
   return (
@@ -16,9 +18,7 @@ function App() {
           <Route path="/products" element={<Products/>}/>
           <Route path="/todo" element={<Todo/>}/>
           <Route path="/todo1" element={<Todo1/>}/>
-
-          <Route path="*" element={<Login/>}/>
-
+          <Route path="*" element={<Page404/>}/>
         </Routes>
       </BrowserRouter>
     </div>
