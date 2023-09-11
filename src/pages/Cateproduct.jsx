275518fae13b2015d3ced8ba472700d5d02deec6
LeoDomGit <leodomsolar@gmail.com>
2023-09-11 15:11:54 +0700

    C

--- src/pages/Cateproduct.jsx
+++ src/pages/Cateproduct.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from "react-router-dom"
 import Footer from "../components/Footer";
+import { useDispatch, useSelector } from "react-redux";
 import Swal from 'sweetalert2';
 import Navbar from "../components/Navbar";
-import {  } from "../redux/productsSlice";
+import { getProducts } from "../redux/productsSlice";
 function Cateproduct() {
+    const dispatch = useDispatch();
+    const {products,loadingP}= useSelector((state) => state.products);
+    console.log(products);
+    useEffect(()=>{
+        dispatch(getProducts())
+    },[])
   return (
     <>
     <Navbar/>
