6b958264a361bbf7c876334100ea7acd93172aea
LeoDomGit <leodomsolar@gmail.com>
2023-09-12 07:39:15 +0700

    Update Cateproduct.jsx

--- src/pages/Cateproduct.jsx
+++ src/pages/Cateproduct.jsx
@@ -1,23 +1,40 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom"
 import Footer from "../components/Footer";
 import { useDispatch, useSelector } from "react-redux";
 import Swal from 'sweetalert2';
 import Navbar from "../components/Navbar";
 import Carousel from '../components/Carousel';
-import { getProducts,cateProducts } from "../redux/productsSlice";
+import { getProducts, cateProducts } from "../redux/productsSlice";
 function Cateproduct() {
-    const dispatch = useDispatch();
-    const { id } = useParams();
-    const {products,loadingP}= useSelector((state) => state.products);
-    const product = products.filter((item)=>item.idCate === id);
-    useEffect(()=>{
-        dispatch(getProducts())
-    },[])
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const { products, loadingP } = useSelector((state) => state.products);
+  const product = products.filter((item) => item.idCate === id);
+  const [limit ,setLimit]= useState(4);
+  useEffect(() => {
+    dispatch(getProducts())
+  }, [])
   return (
     <>
-    <Navbar/>
-    <Carousel/>
+      <Navbar />
+      <Carousel />
+      <div className="container-fluid">
+        <div className="row w-100">
+          <div className="col-md-2 border-end">
+            <div className="container">
+              <input type="text" className='form-control' placeholder='Tên sản phẩm' />
+              <hr />
+              <input type="number" placeholder='Số tiền' className="form-control" />
+            </div>
+          </div>
+          <div className="col-md">
+            <div className="row w-100 ">
+
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
