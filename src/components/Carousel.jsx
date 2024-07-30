5d2c0ad61709d44bb7d2b3701a6f90b627ed8beb
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-07-30 11:46:50 +0700

    Commit

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,36 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import banner1 from '../images/banner AirPods Pro 2 T6_PC.jpg';
+import banner2 from '../images/banner iPad Air 5 T6_PC.jpg';
+import banner3 from '../images/banner iPad_PC.png';
+import banner4 from '../images/banner iPhone 15 Pro_PC.png';
+import banner5 from '../images/banner mac_PC.png';
+import banner6 from '../images/banner MacBook Air M1 T6_PC.jpg';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Autoplay } from 'swiper/modules';
+
+const sliders = [
+    { mobile: 'banner AirPods Pro 2 T6_PC.jpg', desktop: 'banner AirPods Pro 2 T6_PC.jpg' },
+    { mobile: 'banner iPad Air 5 T6_PC.jpg', desktop: 'banner iPad Air 5 T6_PC.jpg' },
+    { mobile: 'banner iPad_PC.png', desktop: 'banner iPad_PC.png' },
+    { mobile: 'banner iPhone 15 Pro_PC.png', desktop: 'banner iPhone 15 Pro_PC.png' },
+    { mobile: 'banner mac_PC.png', desktop: 'banner mac_PC.png' },
+    { mobile: 'banner MacBook Air M1 T6_PC.jpg', desktop: 'banner MacBook Air M1 T6_PC.jpg' }
+];
 
 function Carousel() {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 900);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 900);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <>
-            <div id="carouselExampleIndicators" className="carousel slide mb-3" data-bs-ride="carousel">
-                <div className="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src="https://shopdunk.com/images/uploaded/banner/thang%209%202023/banner%20iphone%2014%20Pro%20Max%20T9_Banner%20PC.jpg" className="d-block w-100" alt="..." />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://shopdunk.com/images/uploaded/banner/thang%209%202023/banner%20ipad%20gen9%20T9_Banner%20PC.jpg" className="d-block w-100" alt="..." />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://shopdunk.com/images/uploaded/banner/thang%209%202023/banner%20watch%20T9_Banner%20PC.jpg" className="d-block w-100" alt="..." />
-                    </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
-            </div>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={1}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                loop={true}
+                modules={[Autoplay]}
+            >
+                {isMobile
+                    ? sliders.map((item, index) => (
+                          <SwiperSlide key={index}>
+                              <img className='w-100' src={`${process.env.REACT_APP_IMG_URL}slides/${item.mobile}`} alt="" />
+                          </SwiperSlide>
+                      ))
+                    : sliders.map((item, index) => (
+                          <SwiperSlide key={index}>
+                              <img className='w-100' src={`${process.env.REACT_APP_IMG_URL}slides/${item.desktop || item.mobile}`} alt="" />
+                          </SwiperSlide>
+                      ))}
+            </Swiper>
         </>
-    )
+    );
 }
 
-export default Carousel
+export default Carousel;
